1d46eac73b2bb8321fcc18a5fea6831d5a342878
Nathaniel Long <nathaniel.s.long@gmail.com>
2017-02-14 17:48:39 -0500

    basic sign up form with routing install, to be tested still

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ get '/sign-in' do
   erb :sign_in
 end
 
-post "/sign-in" do
+post '/sign-in' do
   @user = User.where(email: params[:email]).first
   if @user && @user.password == params[:password]
     session[:user_id] = @user.id
@@ -23,11 +23,30 @@ post "/sign-in" do
   else
     flash[:notice] = "There was a problem with sign-in."
   end
-  redirect "/"
+  redirect '/'
 end
 
-get "/sign-out" do
+get '/sign-out' do
   session.clear
   flash[:notice] = "You've signed out!"
-  redirect "/"
+  redirect '/'
+end
+
+get '/sign-up' do
+  erb :sign_up
+end
+
+post '/sign-up' do
+  @user = User.new
+  @user.email = params[:email]
+  @user.password = params[:password]
+  @user.fname = params[:fname]
+  @user.lname = params[:lname]
+  @user.birthday = params[:birthday]
+  @user.city = params[:city]
+  @user.country = params[:country]
+  @user.save
+  session[:user_id] = @user.id
+  flash[:notice] = "You've signed up successfully!"
+  redirect '/'
 end
