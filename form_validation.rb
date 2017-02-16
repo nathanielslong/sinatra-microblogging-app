9dad7a6c8c8f776ec236a84bc7ef6541977031a8
Nathaniel Long <nathaniel.s.long@gmail.com>
2017-02-16 15:41:23 -0500

    added validations for various pages using activerecod modeling

--- form_validation.rb
+++
@@ -1,3 +0,0 @@
-def form_validation
-
-end
--- models.rb
+++ models.rb
@@ -1,10 +1,15 @@
 class User < ActiveRecord::Base
   has_many :posts
   has_many :groups, through: :user_groups
+
+  validates :email, format: { with: /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/  }
+  validates_length_of :password, minimum: 7
 end
 
 class Post < ActiveRecord::Base
   belongs_to :user
+
+  validates :body, length: { maximum: 150 }
 end
 
 class Group < ActiveRecord::Base
